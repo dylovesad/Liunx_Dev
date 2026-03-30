470339fc7dfe311edecef2106af25430b5903390
dylovesad <150494866+dylovesad@users.noreply.github.com>
2026-03-30 14:59:55 +0800

    Update Delay.c

--- Embedded_system/Code/Delay.c
+++ Embedded_system/Code/Delay.c
@@ -1,8 +1,16 @@
+#if __HAL_TIM_SET_COUNTER(&htim4,0);
+	__HAL_TIM_SET_COUNTER(__HANDLE__, __COUNTER__)  ((__HANDLE__)->Instance->CNT = (__COUNTER__))	
+	__HAL_TIM_GET_COUNTER(&htim4);
+	__HAL_TIM_SET_COUNTER(__HANDLE__, __COUNTER__)  ((__HANDLE__)->Instance->CNT = (__COUNTER__))
+#endif
+
 void Delay_us(uint32_t us){
 	HAL_TIM_Base_Start(&htim4);
-	//__HAL_TIM_SET_COUNTER(&htim4,0);
+	
+
+		
 	htim4.Instance->CNT = 0;
-	//__HAL_TIM_GET_COUNTER(&htim4);
+	
 	while(htim4.Instance->CNT< us){
 	}
 	HAL_TIM_Base_Stop(&htim4);
