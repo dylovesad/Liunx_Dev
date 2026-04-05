d8c1b298b46862e6bfc5e7703dabc7ee35d91c92
dylovesad <150494866+dylovesad@users.noreply.github.com>
2026-04-05 16:38:20 +0800

    Add files via upload

---
+++ code2/sprintf.cpp
@@ -0,0 +1,18 @@
+/*
+    @author: 
+    @date: 
+    @description: sprintf.cpp
+    @note:
+
+    int sprintf(char *str, const char *format, ...) //将格式字符串输出到指定内存上
+    int fprintf(FILE *stream, const char *format, ...)  //将格式字符串输出到指定流上
+*/
+#include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+#include <cstring>
+
+int main(int argc, char *argv[])
+{
+    /*不做展示了*/
+}
